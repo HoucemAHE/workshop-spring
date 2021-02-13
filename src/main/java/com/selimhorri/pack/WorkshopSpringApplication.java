aa6738d541a877bfa7506d824828ab814989ffd4
HoucemAHE <a.houce@outlook.com>
2021-02-13 12:56:57 +0100

    spring boot project initiliazed

---
+++ src/main/java/com/selimhorri/pack/WorkshopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/selimhorri/pack/controllers/StudentController.java
@@ -0,0 +1,16 @@
+package com.selimhorri.pack.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StudentController {
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String displayHelloWorld() {
+		return "Hello World";
+	}
+
+}
